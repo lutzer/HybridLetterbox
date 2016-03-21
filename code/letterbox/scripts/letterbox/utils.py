074c4104a8c9128585b4c2a38af519307573577c
lutzer <lutz@Lutzs-MacBook-Pro.local>
2016-03-21 16:53:37 +0100

    reorganised master branch, added camera calibration code

--- code/letterbox/scripts/letterbox/utils.py
+++
@@ -1,6 +0,0 @@
-from random import randrange
-
-def generateImageName(time,letterboxId):
-	#print(time);
-	timestring = time.strftime('%Y-%m-%d_%H-%M-%S')
-	return letterboxId+'_'+timestring+'_'+str(randrange(1000))
--- code/letterbox/scripts/test/opencv-test.py
+++
@@ -1,5 +0,0 @@
-import cv2
-
-image = cv2.imread("test.png");
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("result.png",image)
