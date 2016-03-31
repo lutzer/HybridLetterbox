318fed6deabd1444f9c39667762551f6a7a34e23
in_the_fishtank@gmx.de <in_the_fishtank@gmx.de>
2016-03-31 17:48:25 +0200

    added testcripts

--- code/pi/scripts/tests/__main__.py
+++ code/pi/scripts/tests/__main__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # @Author: Lutz Reiter, Design Research Lab, Universität der Künste Berlin
-# @Date:   2016-03-31 15:54:56
-# @Last Modified by:   lutzer
-# @Last Modified time: 2016-03-31 15:59:36
+# @Date:   2016-03-31 16:09:53
+# @Last Modified by:   lutz
+# @Last Modified time: 2016-03-31 16:10:09
 
-from tests import cameraPreview
+from test_camera import cameraPreview
 
 # call test functions
 cameraPreview()
