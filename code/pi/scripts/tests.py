63a9e6b132f39013218549243405b57bf7d537f4
lutzer <lutz@Lutzs-MacBook-Pro.local>
2016-03-31 15:59:56 +0200

    write first test

--- code/pi/scripts/tests.py
+++ code/pi/scripts/tests.py
@@ -2,4 +2,9 @@
 # @Author: Lutz Reiter, Design Research Lab, Universität der Künste Berlin
 # @Date:   2016-03-31 15:54:56
 # @Last Modified by:   lutzer
-# @Last Modified time: 2016-03-31 15:54:56
+# @Last Modified time: 2016-03-31 15:59:36
+
+from tests import cameraPreview
+
+# call test functions
+cameraPreview()
--- code/pi/scripts/tests/test_camera.py
+++ code/pi/scripts/tests/test_camera.py
@@ -2,9 +2,10 @@
 # @Author: Lutz Reiter, Design Research Lab, Universität der Künste Berlin
 # @Date:   2016-03-31 15:55:35
 # @Last Modified by:   lutzer
-# @Last Modified time: 2016-03-31 15:56:44
+# @Last Modified time: 2016-03-31 15:59:15
 
 from ..camera.CameraControl import *
 
-camera = CameraControl()
-camera.startPreview()
+def cameraPreview():
+	camera = CameraControl()
+	camera.startPreview()
