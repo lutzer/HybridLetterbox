984782faeb7d4f348ace051955c7650d5625fadc
lutzer <t@t.t>
2016-06-15 18:22:25 +0200

    fixed socket url adress for www app

--- src/server/www/src/config.js
+++ src/server/www/src/config.js
@@ -4,12 +4,12 @@
 * @Author: Lutz Reiter, Design Research Lab, Universität der Künste Berlin
 * @Date:   2016-05-04 11:38:41
 * @Last Modified by:   lutzer
-* @Last Modified time: 2016-05-31 15:05:58
+* @Last Modified time: 2016-06-15 18:18:25
 */
 
 export default {
 	web_service_url : '/api/',
-	web_socket_url : 'http://localhost:'+window.location.port,
+	web_socket_url : '/'+window.location.port,
 	files_url : '/files/',
 	stringTruncateShort: 50,
 	stringTruncateLong: 220,
