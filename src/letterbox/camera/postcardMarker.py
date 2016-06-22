3cdc3aaef2b295543bfd5fe375c4ca7479e52517
lutzer <q@q.de>
2016-06-22 11:57:20 +0200

    changed postcard markers

--- src/letterbox/camera/postcardMarker.py
+++ src/letterbox/camera/postcardMarker.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: Lutz Reiter, Design Research Lab, Universität der Künste Berlin
 # @Date:   2016-03-30 17:41:12
-# @Last Modified by:   lutzer
-# @Last Modified time: 2016-06-22 11:07:13
+# @Last Modified by:   lutz
+# @Last Modified time: 2016-06-22 11:56:55
 
 import cv2
 import numpy as np
@@ -22,12 +22,11 @@ INNER_PATTERNS = [
 ]
 
 OUTER_PATTERN = [
-	[0 , 0 , 0 , 0 , 0, 0],
-	[0 , 1 , 1 , 1 , 1, 0],
-	[0 , 1 , 1 , 1 , 1, 0],
-	[0 , 1 , 1 , 1 , 0, 0],
-	[0 , 1 , 1 , 0 , 1, 0],
-	[0 , 0 , 0 , 0 , 0, 0]
+	[0 , 0 , 0 , 0 , 0],
+	[0 , 1 , 1 , 1 , 0],
+	[0 , 1 , 1 , 0 , 0],
+	[0 , 1 , 0 , 1 , 0],
+	[0 , 0 , 0 , 0 , 0]
 ]
 
 class PostcardMarker:
@@ -39,11 +38,14 @@ class PostcardMarker:
 
 		# put in inner pattern
 		self.pattern[1:3, 1:3] = self.innerPattern
-		self.pattern[3:5, 1:3] = self.innerPattern
-		self.pattern[1:3, 3:5] = self.innerPattern
+
+		# put in parity bit
+		if id == 1:
+			self.pattern[1,3] = 1
+		elif id == 2:
+			self.pattern[3,1] = 1
+		elif id == 3:
+			self.pattern[1,3] = 1
+			self.pattern[3,1] = 1
 
 		self.pattern[self.pattern > 0] = 255
-
-	
-
-	
