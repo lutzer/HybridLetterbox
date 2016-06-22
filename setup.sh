07f8f6b07c86e9eae54ce91e23b0e4aee2af36c0
lutzer <t@t.t>
2016-06-22 14:43:37 +0200

    wrote setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,24 @@
+REPO_DIR=~/HybridLetterbox
+
+# copy ini file
+cp $REPO_DIR/src/letterbox/letterbox.default.ini $REPO_DIR/src/letterbox/letterbox.ini
+
 # copy service file
-cp install/letterbox.service /etc/systemd/system/letterbox.service;
+cp $REPO_DIR/install/letterbox.service /etc/systemd/system/letterbox.service;
 
-# enable and start service
+# enable letterbox service
 sudo systemctl enable letterbox.service;
-sudo systemctl start letterbox.service;
 
-# start server
+# install node modules
+cd $REPO_DIR/src/server/node/;
+npm install;
+
+# copy config file
+cp $REPO_DIR/src/server/node/config.default.js $REPO_DIR/src/server/node/config.js
+
+# start server and save config
+pm2 start $REPO_DIR/src/server/node/main.js --name hybrid-letterbox;
+pm2 save;
+
+# start letterbox service
+sudo systemctl start letterbox.service;
