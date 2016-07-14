21b845fc3e4ddab621238e556d5606aa37f9fe85
lutzer <t@t.t>
2016-07-14 16:35:27 +0200

    disabled auth

--- src/server/node/router/_authentification.js
+++ src/server/node/router/_authentification.js
@@ -4,7 +4,7 @@
 * @Author: Lutz Reiter, Design Research Lab, Universität der Künste Berlin
 * @Date:   2016-05-09 13:17:54
 * @Last Modified by:   lutzer
-* @Last Modified time: 2016-05-09 13:42:45
+* @Last Modified time: 2016-07-14 16:35:19
 */
 
 var httpauth = require('http-auth');
@@ -12,7 +12,8 @@ var httpauth = require('http-auth');
 var basicAuth = httpauth.basic({
     realm: "Admin"
 }, function (username, password, callback) {
-    callback(username === Config.authName && password === Config.authPassword);
+	callback(true);
+    //callback(username === Config.authName && password === Config.authPassword);
 });
 
 module.exports = {
